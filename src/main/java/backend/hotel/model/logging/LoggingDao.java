58c837e51a7ee7b4a8f846687c3032a5d614c500
KennyShow <thoroughgoingkill@gmail.com>
2022-06-20 01:23:27 +0800

    Log系統成功  sql句子區分大小寫

--- src/main/java/backend/hotel/model/logging/LoggingDao.java
+++ src/main/java/backend/hotel/model/logging/LoggingDao.java
@@ -7,27 +7,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+
 @Repository
 @Transactional
 public class LoggingDao implements LoggingInterFaceDao {
 	@Autowired
 	private SessionFactory sessionFactory;
-
 	@Override
-	public boolean checklog(Logging log) {
+	public boolean checklog(Logging users) {
 		Session session = sessionFactory.openSession();
-		String hqlstr = "from Logging where username=:user and user=:pwd";
-		Query<Logging> query = session.createQuery(hqlstr,Logging.class);
-		query.setParameter("user", log.getUsername());
-		query.setParameter("pwd",  log.getUserpwd());
-		Logging logging = query.uniqueResult();
+		String hqlsrt ="from Logging where username=:user and userpwd=:pwd";
+		 Query<Logging> query = session.createQuery(hqlsrt,Logging.class);
+		query.setParameter("user",users.getUsername());
+		query.setParameter("pwd",users.getUserpwd());
+		Logging account = query.uniqueResult();
 		session.close();
 		
-		if (logging!=null) {
+		if (account!=null) {
 			return true;
 		}
 		return false;
 	}
 	
+	
 
 }
+
--- src/main/java/backend/hotel/model/logging/LoggingInterFaceDao.java
+++ src/main/java/backend/hotel/model/logging/LoggingInterFaceDao.java
@@ -2,4 +2,5 @@ package backend.hotel.model.logging;
 
 public interface LoggingInterFaceDao {
 	public boolean checklog(Logging users);
+
 }
